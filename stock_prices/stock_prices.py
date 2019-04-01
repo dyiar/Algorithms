e61a892bd56121e9aaa31c3f76fa92ae4811e0eb
dyiar <austin.hl.blake@gmail.com>
2019-04-01 14:16:35 -0600

    finished stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -6,7 +6,36 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # best_profit = 0
+
+  # for i in range(0, len(prices)):
+  #   for j in range(i+1, len(prices)):
+  #     best_profit = max(best_profit, prices[j] - prices[i])
+
+  # return best_profit
+
+  if len(prices) == 0:
+    return 0
+
+  profit = 0
+  cheapest = prices[0]
+
+  for i in range(1, len(prices)):
+    cheapest = min(cheapest, prices[i])
+
+    profit = max(profit, prices[i]-cheapest)
+
+    max_profit = profit
+
+  if max_profit == 0:
+    return -(prices[len(prices)-1])
+  else:
+    return max_profit
+
+
+
+  
+
 
 
 if __name__ == '__main__':
